42296e0cab90f86f57d483080a8f2b01f774d1b4
Noe <cntabarez@hotmail.com>
2016-11-04 15:12:37 -0300

    Cambio los ROLES a la carpeta que tienen que estar

--- ClinicaFrba/ClinicaFrba/AbmRol/crearRol.Designer.cs
+++ ClinicaFrba/ClinicaFrba/AbmRol/crearRol.Designer.cs
@@ -111,7 +111,7 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form2
+            // crearRol
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -122,8 +122,8 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "Form2";
-            this.Text = "Crear Rol";
+            this.Name = "crearRol";
+            this.Text = "Crear un Rol";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
