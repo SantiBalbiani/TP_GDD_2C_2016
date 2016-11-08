83114e0e8e26f76294b1ec4660205a3344672d0a
SantiBalbiani <santiagobalbiani@yahoo.com>
2016-11-08 00:42:17 -0300

    Avanzo solicitudTurno

--- ClinicaFrba/ClinicaFrba/Pedir Turno/FrmSolTurno.Designer.cs
+++ ClinicaFrba/ClinicaFrba/Pedir Turno/FrmSolTurno.Designer.cs
@@ -31,6 +31,10 @@
             this.button1 = new System.Windows.Forms.Button();
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -41,10 +45,11 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Confirmar Solicitud";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // monthCalendar1
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(161, 18);
+            this.monthCalendar1.Location = new System.Drawing.Point(346, 30);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 2;
             this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
@@ -58,16 +63,49 @@
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(42, 125);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(148, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "Seleccione Profesional";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(42, 41);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(148, 21);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.Text = "Seleccione Especialidad";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(170, 204);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 6;
+            // 
             // FrmSolTurno
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(601, 496);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(this.button1);
             this.Name = "FrmSolTurno";
             this.Text = "Solicitud de Turno";
+            this.Load += new System.EventHandler(this.FrmSolTurno_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -77,5 +115,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.MonthCalendar monthCalendar1;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- ClinicaFrba/ClinicaFrba/Pedir Turno/FrmSolTurno.cs
+++ ClinicaFrba/ClinicaFrba/Pedir Turno/FrmSolTurno.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClinicaFrba.Base_de_Datos;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace ClinicaFrba.Pedir_Turno
 {
@@ -45,6 +48,70 @@ namespace ClinicaFrba.Pedir_Turno
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmSolTurno_Load(object sender, EventArgs e)
+        {
+            Conexion.conectar();
+
+            DataTable especialidades = new DataTable();
+            string cadena = "select idTipo, descripcion from SELECT_GROUP.Tipo_Especialidad";
+
+            especialidades = Conexion.LeerTabla(cadena);
+
+            foreach (DataRow fila in especialidades.Rows)
+            {
+                ComboboxItem item = new ComboboxItem();
+                item.Text = fila["descripcion"].ToString();
+                item.Value = fila["idTipo"];
+                comboBox2.Items.Add(item);
+                comboBox2.SelectedIndex = 0;
+
+
+            }
+            Conexion.conexion.Close();
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Conexion.conectar();
+
+            DataTable profesionalesDeUnaEspecialidad = new DataTable();
+            string cadena = "select profesional_idProfesional from SELECT_GROUP.Profesional_Por_Especialidad WHERE especialidad_idEspecialidad = '" + comboBox2.SelectedIndex.ToString() + "'"; 
+
+            profesionalesDeUnaEspecialidad = Conexion.LeerTabla(cadena);
+            DataTable unProfesionaDeUnaEspecialidad = new DataTable();
+
+            foreach (DataRow filaProfId in profesionalesDeUnaEspecialidad.Rows)
+            {
+
+                string cadena2 = "select nombre, apellido from SELECT_GROUP.Profesional WHERE matricula = '" + filaProfId["profesional_idProfesional"].ToString() + "'";
+                
+                unProfesionaDeUnaEspecialidad = Conexion.LeerTabla(cadena2);
+                foreach (DataRow filaNombreProf in unProfesionaDeUnaEspecialidad.Rows){
+
+
+                    string profesional = filaNombreProf["nombre"].ToString() + ", " + filaNombreProf["apellido"].ToString();
+                    ComboboxItem itemProf = new ComboboxItem();
+
+                    itemProf.Text = profesional;
+                    itemProf.Value = filaProfId["profesional_idProfesional"].ToString();
+
+                    comboBox1.Items.Add(itemProf);
+
+                }
+
+            }
+            Conexion.conexion.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+   
         }
     }
 }
