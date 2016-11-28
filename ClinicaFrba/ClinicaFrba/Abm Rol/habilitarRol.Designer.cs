58062987bb1e1b8c34ac40a8a417270d9666a051
Macarena Lara <molina.macarenalara@gmail.com>
2016-11-28 16:41:55 -0300

    subo habilitar rol

--- ClinicaFrba/ClinicaFrba/Abm Rol/habilitarRol.Designer.cs
+++ ClinicaFrba/ClinicaFrba/Abm Rol/habilitarRol.Designer.cs
@@ -28,20 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(46, 70);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(336, 109);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Seleccione el/los Rol/es a Habilitar;";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(172, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Habilitar Rol";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // habilitarRol
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(467, 305);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
             this.Name = "habilitarRol";
             this.Text = "Habilitar Rol";
             this.Load += new System.EventHandler(this.habilitarRol_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- ClinicaFrba/ClinicaFrba/Abm Rol/habilitarRol.cs
+++ ClinicaFrba/ClinicaFrba/Abm Rol/habilitarRol.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClinicaFrba.Base_de_Datos;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace ClinicaFrba.Abm_Rol
 {
@@ -18,6 +21,29 @@ namespace ClinicaFrba.Abm_Rol
         }
 
         private void habilitarRol_Load(object sender, EventArgs e)
+        {
+            Conexion.conectar();
+            DataTable roles = new DataTable();
+
+            string consultaStr = "select idRol, nombre from SELECT_GROUP.Rol where rol.habilitado=0";
+
+            roles = Conexion.LeerTabla(consultaStr);
+
+            DataTable nombreFuncionalidades = new DataTable();
+
+
+            foreach (DataRow idRol in roles.Rows)
+            {
+                ComboboxItem unRol = new ComboboxItem();
+
+                unRol.Text = idRol["nombre"].ToString();
+                unRol.Value = idRol["idRol"].ToString();
+
+                checkedListBox1.Items.Add(unRol);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
