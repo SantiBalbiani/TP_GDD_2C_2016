9b45b9b53bbf8a2be6b095455da882caaa6e0fe2
SantiBalbiani <santiagobalbiani@yahoo.com>
2016-11-30 15:36:09 -0300

    cancel turno afil 100% OK

--- ClinicaFrba/ClinicaFrba/Cancelar Atencion/CancelacionAfiliado.cs
+++ ClinicaFrba/ClinicaFrba/Cancelar Atencion/CancelacionAfiliado.cs
@@ -43,7 +43,12 @@ namespace ClinicaFrba.Cancelar_Atencion
                 unItem.Text = turno["fechaTurno"].ToString();
                 unItem.Value = turno["idTurno"].ToString();
 
-                listBox1.Items.Add(unItem);
+                DateTime fecha = DateTime.Parse(unItem.Text.ToString());
+
+                if (fecha > DateTime.Now)
+                {
+                    listBox1.Items.Add(unItem);
+                }
             }
 
             string queryTiposCanc = "Select idTipoCanc, descripcion FROM Select_Group.Tipo_Cancelacion";
@@ -57,7 +62,7 @@ namespace ClinicaFrba.Cancelar_Atencion
 
                 unItem.Text = unTipoCanc["descripcion"].ToString();
                 unItem.Value = unTipoCanc["idTipoCanc"].ToString();
-
+                
                 cmbTipo.Items.Add(unItem);
             }
 
@@ -87,6 +92,33 @@ namespace ClinicaFrba.Cancelar_Atencion
             else 
             {
                 //Llamar SP que impacte en tabla Cancelacion y actualice Turno.estado y Turno.idCancelacion
+                SqlConnection cnx = new SqlConnection(ConfigurationManager.ConnectionStrings["miCadenaConexion"].ConnectionString);
+                SqlCommand cmdUsuario = new SqlCommand("Select_Group.sp_registrarCancelacion", cnx);
+                cmdUsuario.CommandType = CommandType.StoredProcedure;
+                Object tipoMotivo = cmbTipo.SelectedItem;
+                ComboboxItem tipoMotivoelegido = (ComboboxItem)tipoMotivo;
+
+                cmdUsuario.Parameters.Add("@idTipoCanc", SqlDbType.Int).Value = tipoMotivoelegido.Value;
+                cmdUsuario.Parameters.Add("@Motivo", SqlDbType.VarChar).Value = txtMotivo.Text.ToString();
+                cmdUsuario.Parameters.Add("@idTurno", SqlDbType.Int).Value = turnoElegido.Value;
+
+                try
+                {
+
+                    cnx.Open();
+                    cmdUsuario.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    cnx.Close();
+                    HomeAfiliado home = new HomeAfiliado();
+                    home.Show();
+                    this.Close();
+                }
             }
 
              
