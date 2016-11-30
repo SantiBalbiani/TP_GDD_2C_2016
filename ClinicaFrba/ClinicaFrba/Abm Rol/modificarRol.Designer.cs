0106ceb7a2153351b12db0c2023ed005f0bd0a48
Macarena Lara <molina.macarenalara@gmail.com>
2016-11-30 01:16:44 -0300

    modificar rol sin errores

--- ClinicaFrba/ClinicaFrba/Abm Rol/modificarRol.Designer.cs
+++ ClinicaFrba/ClinicaFrba/Abm Rol/modificarRol.Designer.cs
@@ -87,6 +87,7 @@
             this.btnBuscar.TabIndex = 13;
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // modificarRol
             // 
--- ClinicaFrba/ClinicaFrba/Abm Rol/modificarRol.cs
+++ ClinicaFrba/ClinicaFrba/Abm Rol/modificarRol.cs
@@ -27,80 +27,12 @@ namespace ClinicaFrba.AbmRol
 
         private void modificarRol_Load(object sender, EventArgs e)
         {
-            string query = "select r.nombre from SELECT_GROUP.Rol as r";
-            DataTable dt = Conexion.EjecutarComando(query);
-            foreach (DataRow fila in dt.Rows)
-            {
-                comboBox1.Items.Add(Convert.ToString(fila["nombre"]));
-            }  
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            SqlConnection cnx = new SqlConnection(ConfigurationManager.ConnectionStrings["miCadenaConexion"].ConnectionString);
-            SqlCommand cmdUsuario = new SqlCommand("Select_Group.ModificarRol", cnx);
-            cmdUsuario.CommandType = CommandType.StoredProcedure;
-            cmdUsuario.Parameters.Add("@ROL_DESCRIP", SqlDbType.VarChar).Value = comboBox1.Text;
-            cmdUsuario.Parameters.Add("@FUNCIONALIDAD_QUITAR", SqlDbType.VarChar).Value = checkedListBox1.Text;
-       
-            try
-            {
-
-                cnx.Open();
-                cmdUsuario.ExecuteNonQuery();
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                cnx.Close();
-                HomeAfiliado home = new HomeAfiliado();
-                home.Show();
-                this.Close();
-            }
-        }
-
-        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
-        {
-            Conexion.conectar();
-            comboBox1.Items.Clear();
-            comboBox1.ResetText();
+           
         }
 
         private void modificarRol_Load_1(object sender, EventArgs e)
         {
-            Conexion.conectar();
-            DataTable funcionalidades = new DataTable();
-
-            string consultaStr = "select idFuncionalidad, descripcion from SELECT_GROUP.Funcionalidad";
-
-            funcionalidades = Conexion.LeerTabla(consultaStr);
-
-            DataTable nombreFuncionalidades = new DataTable();
-
-
-            foreach (DataRow idFunc in funcionalidades.Rows)
-            {
-                ComboboxItem unaFuncionalidad = new ComboboxItem();
-
-                unaFuncionalidad.Text = idFunc["descripcion"].ToString();
-                unaFuncionalidad.Value = idFunc["idFuncionalidad"].ToString();
-
-                checkedListBox1.Items.Add(unaFuncionalidad);
-            }
-
-            DataTable nombreFuncionalidades2 = new DataTable();
-                foreach (DataRow idFunc2 in funcionalidades.Rows)
-                {
-                    ComboboxItem unaFuncionalidad2 = new ComboboxItem();
-
-                    unaFuncionalidad2.Text = idFunc2["descripcion"].ToString();
-                    unaFuncionalidad2.Value = idFunc2["idFuncionalidad"].ToString();
-
-                    checkedListBox2.Items.Add(unaFuncionalidad2);
-                }               
+            
 
 
             
@@ -112,6 +44,11 @@ namespace ClinicaFrba.AbmRol
         }
 
         private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
         {
 
         }
