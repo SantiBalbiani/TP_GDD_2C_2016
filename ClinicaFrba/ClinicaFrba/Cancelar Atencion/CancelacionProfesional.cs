6e6ee0fb6a3d877bf1629c10f4c1676efeb82fbe
SantiBalbiani <santiagobalbiani@yahoo.com>
2016-11-30 19:19:22 -0300

    Subo CancelacionTurnoProf 100% OK

--- ClinicaFrba/ClinicaFrba/Cancelar Atencion/CancelacionProfesional.cs
+++ ClinicaFrba/ClinicaFrba/Cancelar Atencion/CancelacionProfesional.cs
@@ -15,13 +15,16 @@ namespace ClinicaFrba.Cancelar_Atencion
 {
     public partial class CancelacionProfesional : Form
     {
-        public CancelacionProfesional()
+        public CancelacionProfesional(String matricula)
         {
             InitializeComponent();
+            idProf = matricula;
+
         }
         private bool eligioFecha = false;
         private bool eligioTipo = false;
         private bool escibioMotivo = false;
+        private string idProf;
 
         private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
         {
@@ -40,7 +43,49 @@ namespace ClinicaFrba.Cancelar_Atencion
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
+
+            DateTime UltFecha = Calendar.SelectionEnd;
+            
+       
+            double diasAnticipacion = (UltFecha - DateTime.Now).TotalDays;
+            if (diasAnticipacion < 1)
+            {
+                MessageBox.Show("Solo se pueden cancelar turnos con 1 día de anticipación");
+            }
+            else 
+            {
+                
+                SqlConnection cnx = new SqlConnection(ConfigurationManager.ConnectionStrings["miCadenaConexion"].ConnectionString);
+                SqlCommand cmdUsuario = new SqlCommand("Select_Group.sp_cancelacionProfesional", cnx);
+                cmdUsuario.CommandType = CommandType.StoredProcedure;
+                Object tipoMotivo = cmbTipo.SelectedItem;
+                ComboboxItem tipoMotivoelegido = (ComboboxItem)tipoMotivo;
+
+                cmdUsuario.Parameters.Add("@tipoMot", SqlDbType.Int).Value = tipoMotivoelegido.Value;
+                cmdUsuario.Parameters.Add("@Motivo", SqlDbType.VarChar).Value = txtMotivo.Text.ToString();
+                cmdUsuario.Parameters.Add("@idProf", SqlDbType.Int).Value = idProf;
+                cmdUsuario.Parameters.Add("@fechaDesde", SqlDbType.DateTime).Value = Calendar.SelectionStart;
+                cmdUsuario.Parameters.Add("@fechaHasta", SqlDbType.DateTime).Value = UltFecha;
+
+                try
+                {
+
+                    cnx.Open();
+                    cmdUsuario.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    cnx.Close();
+                    MessageBox.Show("Se cancelaron todos los turnos pertenecientes a los días seleccionados");
+                    Menu_Principal.HomeProfesional home = new Menu_Principal.HomeProfesional();
+                    home.Show();
+                    this.Close();
+                }
+            }
         }
 
         private void txt1_TextChanged(object sender, EventArgs e)
--- ClinicaFrba/ClinicaFrba/Menu Principal/HomeProfesional.cs
+++ ClinicaFrba/ClinicaFrba/Menu Principal/HomeProfesional.cs
@@ -8,11 +8,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ClinicaFrba.Registro_Resultado;
+using ClinicaFrba.Base_de_Datos;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace ClinicaFrba.Menu_Principal
 {
     public partial class HomeProfesional : Form
     {
+        private string nombreProf;
+        private string apellidoProf;
+        private string idProf;
         public HomeProfesional()
         {
             InitializeComponent();
@@ -39,6 +45,22 @@ namespace ClinicaFrba.Menu_Principal
 
         private void HomeProfesional_Load(object sender, EventArgs e)
         {
+            string queryDatosProf = "SELECT matricula ,nombre ,apellido FROM Select_Group.Profesional P JOIN Select_Group.Usuario U ON U.idUsuario = P.idUsuario AND U.nombreUsuario = '"+ Globals.userName+"'";
+
+            DataTable datosProf = new DataTable();
+
+            Conexion.conectar();
+
+            datosProf = Conexion.LeerTabla(queryDatosProf);
+
+            foreach (DataRow datosUnProf in datosProf.Rows)
+            {
+                idProf = datosUnProf["matricula"].ToString();
+                 nombreProf = datosUnProf["nombre"].ToString();
+                 apellidoProf= datosUnProf["apellido"].ToString();
+            }
+
+            label3.Text = apellidoProf + ", " + nombreProf;
 
         }
 
@@ -49,6 +71,15 @@ namespace ClinicaFrba.Menu_Principal
 
         private void label2_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Cancelar_Atencion.CancelacionProfesional frmCancelarDia = new Cancelar_Atencion.CancelacionProfesional(idProf);
+            frmCancelarDia.Show();
+            this.Close();
+
 
         }
     }
