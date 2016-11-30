b6c98eba715fa8f870f0e32438dab0aff3fe407a
SantiBalbiani <santiagobalbiani@yahoo.com>
2016-11-30 13:37:08 -0300

    CancelacionTurnoAfiliado:Anda cargar Turnos

--- ClinicaFrba/ClinicaFrba/Cancelar Atencion/CancelacionAfiliado.cs
+++ ClinicaFrba/ClinicaFrba/Cancelar Atencion/CancelacionAfiliado.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClinicaFrba.Base_de_Datos;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace ClinicaFrba.Cancelar_Atencion
 {
@@ -21,7 +24,21 @@ namespace ClinicaFrba.Cancelar_Atencion
         
         private void CancelacionAfiliado_Load(object sender, EventArgs e)
         {
-            
+            string consultarTurnosParaProfYAfiliado = "SELECT T.idTurno ,T.idAgenda ,T.fechaTurno ,T.afiliado_idAfiliado  FROM Select_Group.Turno T WHERE T.estado = 3 AND T.afiliado_idAfiliado = " + idAfiliado;
+            Conexion.conectar();
+            DataTable Turnos = new DataTable();
+
+            Turnos = Conexion.LeerTabla(consultarTurnosParaProfYAfiliado);
+
+            foreach (DataRow turno in Turnos.Rows)
+            {
+                ComboboxItem unItem = new ComboboxItem();
+
+                unItem.Text = turno["fechaTurno"].ToString();
+                unItem.Value = turno["idTurno"].ToString();
+
+                listBox1.Items.Add(unItem);
+            }
         }
     }
 }
