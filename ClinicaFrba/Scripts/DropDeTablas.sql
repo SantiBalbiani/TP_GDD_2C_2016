cadde1f9b5eab3b850f0c4aed37f6afcc7f1951a
gutilorenzo <gutilorenzo@est.frba.utn.edu.ar>
2016-12-01 20:23:20 -0300

    Subo archivos borrados sin querer.

---
+++ ClinicaFrba/Scripts/DropDeTablas.sql
@@ -0,0 +1,30 @@
+USE [GD2C2016]
+GO
+
+/****** Object:  Table [Select_Group].[Afiliado]    Script Date: 30/10/2016 21:04:52 ******/
+DROP TABLE [Select_Group].[Usuario_Por_Rol]
+DROP TABLE [Select_Group].[Afiliado]
+DROP TABLE [Select_Group].[Agenda]
+DROP TABLE [Select_Group].[Bono]
+DROP TABLE [Select_Group].[Diagnostico]
+DROP TABLE [Select_Group].[Agenda_detalle]
+DROP TABLE [Select_Group].[Cancelacion]
+DROP TABLE [Select_Group].[Compras]
+DROP TABLE [Select_Group].[Especialidad]
+DROP TABLE [Select_Group].[Estado_Turno]
+DROP TABLE [Select_Group].[Funcionalidad]
+DROP TABLE [Select_Group].[Funcionalidad_Por_Rol]
+DROP TABLE [Select_Group].[Plan_Historico]
+DROP TABLE [Select_Group].[Plann]
+DROP TABLE [Select_Group].[Profesional]
+DROP TABLE [Select_Group].[Profesional_Por_Especialidad]
+DROP TABLE [Select_Group].[Rol]
+DROP TABLE [Select_Group].[Tipo_Cancelacion]
+DROP TABLE [Select_Group].[Tipo_Especialidad]
+DROP TABLE [Select_Group].[Turno]
+DROP TABLE [Select_Group].[Usuario]
+DROP TABLE [Select_Group].[Plan_Med]
+
+GO
+
+
