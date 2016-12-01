484827577bb995fc1e0650e2be8c1ffa60fb505e
SantiBalbiani <santiagobalbiani@yahoo.com>
2016-11-30 23:21:49 -0300

    Corrijo Query y adapto formato

--- ClinicaFrba/ClinicaFrba/Listados/ListadoEstadistico.cs
+++ ClinicaFrba/ClinicaFrba/Listados/ListadoEstadistico.cs
@@ -43,7 +43,7 @@ namespace ClinicaFrba.Listados
             //string contador;
 
             DataTable Lista = new DataTable();
-            string cadena = "SELECT TOP 5 P.matricula matricula, count(Af.plan_idPlan) veces FROM Select_Group.Profesional P JOIN Select_Group.Agenda Ag ON Ag.profesional_IdProfesional = P.matricula JOIN Select_Group.Turno T ON T.idAgenda = Ag.idAgenda JOIN Select_Group.Afiliado Af ON Af.idAfiliado = T.afiliado_idAfiliado GROUP BY P.matricula, Af.plan_idPlan ORDER BY count(Af.plan_idPlan)";
+            string cadena = "SELECT TOP 5 P.matricula , P.apellido, P.nombre, E.descripcion FROM Select_Group.Profesional P JOIN Select_Group.Profesional_Por_Especialidad PE ON PE.profesional_idProfesional = P.matricula JOIN Select_Group.Especialidad E ON E.idEspecialidad = PE.especialidad_idEspecialidad JOIN Select_Group.Agenda Ag ON Ag.profesional_IdProfesional = P.matricula  JOIN Select_Group.Turno T ON T.idAgenda = Ag.idAgenda JOIN Select_Group.Afiliado Af ON Af.idAfiliado = T.afiliado_idAfiliado GROUP BY P.matricula, Af.plan_idPlan, E.descripcion, P.apellido, P.nombre  ORDER BY count(Af.plan_idPlan)";
             Lista = Conexion.LeerTabla(cadena);
             listView1.Clear();
 
@@ -52,23 +52,27 @@ namespace ClinicaFrba.Listados
             listView1.FullRowSelect = true;
 
             listView1.Columns.Add("Matricula", 100);
-            listView1.Columns.Add("Cantidad Veces", 100);
+            listView1.Columns.Add("Apellido", 100);
+            listView1.Columns.Add("Nombre", 100);
+            listView1.Columns.Add("Descripcion", 200);
             // listView1.Columns.Add("Quantity", 70);
           
             foreach (DataRow listado in Lista.Rows)
             {
 
             string matricula = listado["matricula"].ToString();
-            string veces = listado["veces"].ToString();
-            
+            string apellido = listado["Apellido"].ToString();
+            string nombre = listado["Nombre"].ToString();
+            string descripcion = listado["Descripcion"].ToString();
             //Add items in the listview
             string[] arr = new string[4];
             ListViewItem itm ;
 
             //Add first item
             arr[0] = matricula;
-            arr[1] = veces;
-            arr[2] = "10";
+            arr[1] = apellido;
+            arr[2] = nombre;
+            arr[3] = descripcion;
             itm = new ListViewItem(arr);
             listView1.Items.Add(itm);
             }
