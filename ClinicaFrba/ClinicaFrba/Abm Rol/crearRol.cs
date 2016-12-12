6f5ced3f7a457814596a06da605ca5c9046cd8fe
Macarena Lara <molina.macarenalara@gmail.com>
2016-12-11 21:56:02 -0300

    fix issue #56

--- ClinicaFrba/ClinicaFrba/Abm Rol/crearRol.cs
+++ ClinicaFrba/ClinicaFrba/Abm Rol/crearRol.cs
@@ -54,98 +54,104 @@ namespace ClinicaFrba.AbmRol
         private void button1_Click(object sender, EventArgs e)
         {
 
-            //Conexion.conectar();
-            SqlConnection conexion;
-            bool conectado = false;
-            //llenar la variable conexión con los parámetros de la variable parametros
-            string parametros = ConfigurationManager.ConnectionStrings["miCadenaConexion"].ConnectionString;
-            conexion = new SqlConnection(parametros);
-            try
+            if (!String.IsNullOrWhiteSpace(nuevoRol.Text))
             {
-                //abrir la conexion
-                conexion.Open();
-                conectado = true;
-            }
-            catch (InvalidCastException)
-            {
-                MessageBox.Show("Error al conectar la Base de datos");
-                conectado = false;
-            }
 
-
-            if (conectado == true) {
-
+                //Conexion.conectar();
+                SqlConnection conexion;
+                bool conectado = false;
+                //llenar la variable conexión con los parámetros de la variable parametros
+                string parametros = ConfigurationManager.ConnectionStrings["miCadenaConexion"].ConnectionString;
+                conexion = new SqlConnection(parametros);
                 try
                 {
-                    //string str[] = new string(checkedListBox1.CheckedItems.Count);
-                    //Dim str(checkedListBox1.CheckedItems.Count) As string;
-                    //string str[checkedListBox1.CheckedItems.Count] = new String(checkedListBox1.CheckedItems.Count);
-                    if (checkedListBox1.CheckedItems.Count > 0)
+                    //abrir la conexion
+                    conexion.Open();
+                    conectado = true;
+                }
+                catch (InvalidCastException)
+                {
+                    MessageBox.Show("Error al conectar la Base de datos");
+                    conectado = false;
+                }
+
+
+                if (conectado == true)
+                {
+
+                    try
                     {
-
-                        //inserta rol nuevo en la tabla rol 
-                        SqlCommand cmdRol = new SqlCommand("insert into Select_group.Rol (nombre,habilitado) values(@nombreRol,1)", conexion);
-                        cmdRol.Parameters.AddWithValue("@nombreRol", nuevoRol.Text);
-                        cmdRol.ExecuteNonQuery();
-
-                        //Aca poner query que busque el Rol por la descripción y levantar el idRol
-
-                        string buscaRol = "SELECT idRol FROM Select_Group.Rol WHERE nombre = '" + nuevoRol.Text.ToString() + "'";
-
-                        DataTable elRolAgregado = new DataTable();
-                        Conexion.conectar();
-                        elRolAgregado = Conexion.LeerTabla(buscaRol);
-                        string idRol = " ";
-                        foreach (DataRow unRol in elRolAgregado.Rows)
+                        //string str[] = new string(checkedListBox1.CheckedItems.Count);
+                        //Dim str(checkedListBox1.CheckedItems.Count) As string;
+                        //string str[checkedListBox1.CheckedItems.Count] = new String(checkedListBox1.CheckedItems.Count);
+                        if (checkedListBox1.CheckedItems.Count > 0)
                         {
-                            idRol = unRol["idRol"].ToString();
+
+                            //inserta rol nuevo en la tabla rol 
+                            SqlCommand cmdRol = new SqlCommand("insert into Select_group.Rol (nombre,habilitado) values(@nombreRol,1)", conexion);
+                            cmdRol.Parameters.AddWithValue("@nombreRol", nuevoRol.Text);
+                            cmdRol.ExecuteNonQuery();
+
+                            //Aca poner query que busque el Rol por la descripción y levantar el idRol
+
+                            string buscaRol = "SELECT idRol FROM Select_Group.Rol WHERE nombre = '" + nuevoRol.Text.ToString() + "'";
+
+                            DataTable elRolAgregado = new DataTable();
+                            Conexion.conectar();
+                            elRolAgregado = Conexion.LeerTabla(buscaRol);
+                            string idRol = " ";
+                            foreach (DataRow unRol in elRolAgregado.Rows)
+                            {
+                                idRol = unRol["idRol"].ToString();
+                            }
+
+
+
+                            foreach (Object item in checkedListBox1.CheckedItems)
+                            {
+
+                                ComboboxItem unItem = new ComboboxItem();
+
+                                unItem = (ComboboxItem)item;
+
+                                SqlCommand cmdFuncionalidad = new SqlCommand("insert into Select_group.Funcionalidad_Por_Rol (rol_idRol, funcionalidad_idFuncionalidad) values(@idRol,@idFunc)", conexion);
+                                cmdFuncionalidad.Parameters.AddWithValue("@idRol", idRol);//Aca pasar el idRol recuperado previamente
+                                cmdFuncionalidad.Parameters.AddWithValue("@idFunc", unItem.Value);
+                                cmdFuncionalidad.ExecuteNonQuery();
+
+
+                            }
+
+
+
+                            MessageBox.Show("Rol creado con exito con las funcionalidades asignadas ");
+                            Conexion.conexion.Close();
                         }
 
+                        else
+                        {
+                            MessageBox.Show("Porfavor seleccione al menos una Funcionalidad");
+                        }
 
+                        while (checkedListBox1.CheckedItems.Count > 0)
+                        {
+                            checkedListBox1.SetItemChecked(checkedListBox1.CheckedIndices[0], false);
+                        }
 
-                        foreach ( Object item  in checkedListBox1.CheckedItems){
-
-                            ComboboxItem unItem = new ComboboxItem();
-
-                            unItem = (ComboboxItem)item;
-
-                            SqlCommand cmdFuncionalidad = new SqlCommand("insert into Select_group.Funcionalidad_Por_Rol (rol_idRol, funcionalidad_idFuncionalidad) values(@idRol,@idFunc)", conexion);
-                            cmdFuncionalidad.Parameters.AddWithValue("@idRol", idRol);//Aca pasar el idRol recuperado previamente
-                            cmdFuncionalidad.Parameters.AddWithValue("@idFunc", unItem.Value);
-                            cmdFuncionalidad.ExecuteNonQuery();
-
-
-                         }
-
-
-
-                        MessageBox.Show("Rol creado con exito con las funcionalidades asignadas ");
-                        Conexion.conexion.Close();
                     }
-
-                    else
+                    catch (Exception ex)
                     {
-                        MessageBox.Show("Porfavor seleccione al menos una Funcionalidad");
+                        MessageBox.Show(ex.Message);
                     }
-
-                    while (checkedListBox1.CheckedItems.Count > 0)
-                    {
-                        checkedListBox1.SetItemChecked(checkedListBox1.CheckedIndices[0], false);
-                    }
-
                 }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
+
             }
 
-
-
-
-            
+            else
+            {
+                MessageBox.Show("Ingrese un nombre válido");
+            }
         }
-
         private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
